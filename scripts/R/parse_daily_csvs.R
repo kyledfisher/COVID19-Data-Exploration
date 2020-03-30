a592a16eb5dc6629e1bf72b105fc5fa9ce3b3249
Zach Wallace <zach.wallace@live.com>
2020-03-29 20:05:29 -0700

    xml fetching and parsing for csv names

--- scripts/R/parse_daily_csvs.R
+++ scripts/R/parse_daily_csvs.R
@@ -2,8 +2,27 @@
 #
 # Update data/ directory with newly added csvs from the main CSSE repo located 
 # at https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports.
+#
 
 
-file <- readLines(con='/Users/zach/Code/COVID19-Data-Exploration/data/csse_covid_19_daily_reports')
-file[300]
-grep('.csv', file, fixed=TRUE, value=TRUE)
+
+library(magrittr)
+library(xml2)
+
+# Pull in list of daily data. 
+xml.path <- 'https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports'
+report.file <- download_html(xml.path)
+html.read <- read_html(report.file)
+csv.list <- xml_text(html.read) %>% strsplit(split='\\n') %>% unlist
+
+# Parse html for the csv names
+dates.list <- lapply(csv.list, function(x) {
+    if (grepl(pattern='.csv', x, fixed=TRUE)) { 
+        regex <- regexpr('\\d{2}-\\d{2}-\\d{4}.csv', x)
+        return(substr(x, start=regex[[1]], stop=regex[[1]]+attr(regex, 'match.length')))
+    }
+})
+dates.list[sapply(dates.list, is.null)] <- NULL
+dates.list %>% unlist
+
+# Fetch raw data
--- scripts/cpp/fast_reader.cpp
+++ scripts/cpp/fast_reader.cpp
@@ -2,14 +2,34 @@
  * fast_reader.cpp
  * 
  * Read and parse lines in c++ for performance reasons.
- * readLines in R actually quite slow
+ * readLines in R is somewhat slow.
+ * 
+ * TODO: actually make this faster.  Oveheard of calling SourceRCpp negates 
+ *       performance benefit :(
  */
 
 
-#include <iostream.h>
-#include <fstream.h>
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <Rcpp.h>
 
 
- int main() {
+// [[Rcpp::export]]
+std::vector<std::string> fastReadLines() {
 
- }
+    std::ifstream f {"/Users/zach/Code/COVID19-Data-Exploration/data/csse_covid_19_daily_reports"};
+    std::string line;
+    std::vector<std::string> linevec;
+    
+    if (!f) {
+        std::cerr << "Cannot open file for reading" << std::endl;
+    } 
+    else {
+        while(getline(f,line)) {
+            linevec.push_back(line);
+        }
+    }
+    
+    return(linevec);
+}
