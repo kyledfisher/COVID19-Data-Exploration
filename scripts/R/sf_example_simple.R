7dbd368b62c79cc2abdcc0c4c5993741f826362a
Zach Wallace <zach.wallace@live.com>
2020-04-22 15:21:37 -0700

    Add simple sf example

---
+++ scripts/R/sf_example_simple.R
@@ -0,0 +1,27 @@
+# sf_example_simple.R
+
+library(sf)
+library(magrittr)
+library(data.table)
+library(dplyr)
+library(RPostgreSQL)
+library(mapview)
+
+source('C:/Users/zach/Documents/Code/COVID19-Data-Exploration/scripts/R/insert_report_data.R')
+
+# remove NA's 
+report_data.dt <- report_data.dt[!is.na(report_data.dt$latitude) & 
+                                 !is.na(report_data.dt$longitude),]
+
+# Create spatial object from data table and view on global map
+report_data.sf <- st_as_sf(report_data.dt, coords = c("longitude", "latitude"), crs=4326)
+mapview(report_data.sf)
+
+us_data.dt <- report_data.dt[report_data.dt$country_region=='US',]
+us_data.sf <- st_as_sf(us_data.dt, coords = c("longitude", "latitude"), crs=4326)
+mapview(us_data.sf)
+
+# bench::mark(
+#     b1 <- report_data.dt[report_data.dt$country_region=='US',],
+#     b2 <- report_data.sf[report_data.sf$country_region=='US',] %>% data.table
+# )
